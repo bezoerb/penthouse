6444c4450c769ff03ffe70d87f114b102745444f
Ben Zörb <benjamin.zoerb@jvm.com>
2025-09-28 23:37:05 +0200

    Updates puppeteer & jest

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,13 @@ module.exports = {
     '^.+\\.js$': 'babel-jest'
   },
   moduleFileExtensions: ['js'],
-  testMatch: ['**/test/**/*.test.js']
+  testMatch: ['**/test/**/*.test.js'],
+  // Force Jest to exit after tests complete
+  forceExit: true,
+  // Detect open handles to help with debugging
+  detectOpenHandles: true,
+  // Run tests in sequence to avoid worker process issues
+  maxWorkers: 1
 }
 
 
